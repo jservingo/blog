42ec5927a97ddacab604118cb57c7485f7e3913a
Jorge Servin <jservingo@gmail.com>
2019-04-22 20:48:58 -0400

    Storage path

--- resources/views/posts/full/photo.blade.php
+++ resources/views/posts/full/photo.blade.php
@@ -1,6 +1,6 @@
 {{-- posts.full.photo --}}
 
 <div style="width:345px; height:auto; overflow:auto; background-color:#d7e9f3">
-	<img src="{{ url('storage/'.$post->photos->first()->url) }}" 
+	<img src="{{ Storage::url($post->photos->first()->url) }}" 
 			alt="{{ $post->title }}" class="img-responsive ifull">
 </div>
--- resources/views/posts/list/photo_card.blade.php
+++ resources/views/posts/list/photo_card.blade.php
@@ -1,7 +1,7 @@
 {{-- posts.list.photo_card --}}
 
 <div style="width:98px; height:98px; background-color:{{ $zcolor }}">
-	<img src="{{ url('storage/'.$post->photos->first()->url) }}" 
+	<img src="{{ Storage::url($post->photos->first()->url) }}" 
 			alt="{{ $post->title }}" class="img-responsive-card" 
 			width="98">
 </div>
