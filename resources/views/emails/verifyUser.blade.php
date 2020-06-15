d978cb632f4bacbc23c46244e331f90dd7ca6b16
Jorge Servin <jservingo@gmail.com>
2020-06-15 16:04:51 -0400

    verifyUser email v2 and new ads

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -4,10 +4,13 @@
     <title>Kodelia</title>
   </head>
   <body>
-    <h2>Welcome {{ $name }} to the site kodelia</h2>
-    <br/>
-    Your registered email is {{ $email }}, Please click on the below link to verify your email account
-    <br/>
+    <h1>Kodelia</h1>
+    <h2>Registro de usuario {{ $name }}</h2> 
+    <p>Hemos recibido su solicitud de registro en nuestra plataforma. 
+       Por favor, recuerde verificar su identidad para iniciar.
+       Puede verificar su correo electrónico, a continuación</p>
+    <p>Your registered email is {{ $email }}, Please click on the below link to verify your email account</p>
     <a href="{{url('user/verify', $token)}}">Verify Email</a>
+    <p>Si esta solicitud se ha enviado sin su consentimiento, por favor descártela</p>
   </body>
 </html>
