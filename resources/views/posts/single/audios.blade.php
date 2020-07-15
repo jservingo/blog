533ac023e8dd049f532f746097669dbabaa46f61
Jorge Servin <jservingo@gmail.com>
2020-07-15 08:19:41 -0400

    audioWrap

--- resources/views/posts/single/audios.blade.php
+++ resources/views/posts/single/audios.blade.php
@@ -1,9 +1,17 @@
 {{-- posts.single.audios --}}
 
 <div class="audios container-flex">
-	@foreach ($post->audios as $audio)
-		<span class="tag c-gray-1 text-capitalize">
-			{{ $audio->description }}
-		</span>
-	@endforeach
+	<div id="audioWrap"></div>
+		<ul>
+			@foreach ($post->audios as $audio)
+				<li>
+					<a id="btn_play_audio" 
+							data-url="{{ $audio->url }}" 
+							href="#">
+							{{ $audio->description }}
+					</a>
+				</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
