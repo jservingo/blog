80f5179e69d507db8a7d7c0e3f9eeab684749b03
Jorge Servin <jservingo@gmail.com>
2020-07-26 18:29:31 -0400

    fix scope published_at date

--- app/Post.php
+++ app/Post.php
@@ -112,7 +112,7 @@ class Post extends Model
                 $query->where('posts.user_id','<>',auth()->id())
                     ->where('cstr_privacy','=',1)
                     ->whereNotNull('published_at')
-                    ->where('published_at','>=',date('Y-m-d').' 00:00:00');
+                    ->where('published_at','<=',date('Y-m-d').' 00:00:00');
             }); 
         }
     }
