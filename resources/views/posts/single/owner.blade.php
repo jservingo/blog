0cd1ad0a14caac513b896e5084d8f2ddeae59456
Jorge Servin <jservingo@gmail.com>
2020-08-04 20:09:17 -0400

    posts single owner page show catalogs

--- resources/views/posts/single/owner.blade.php
+++ resources/views/posts/single/owner.blade.php
@@ -38,7 +38,7 @@
 			<div><a class="vlink" href="{{ route('posts.show_created_by_user',['user' => $post->user->id, 'type' => 0]) }}">{{ __('messages.posts') }} <span id="u_posts"></a></div>
 		@endif
 		@if ($post->isPage())
-			<div><a class="vlink" href="{{ route('catalogs.show_created_by_user',['user' => $post->user->id]) }}">{{ __('messages.catalogs') }} <span id="p_catalogs"></a></div>
+			<div><a class="vlink" href="{{ route('page.show_page_category',['page' => $page->id, 'category' => 0]) }}">{{ __('messages.catalogs') }} <span id="p_catalogs"></a></div>
 		@endif
 		@if ($post->isCatalog())
 			<div><a class="vlink" href="{{ route('catalog.show_catalog',['catalog' => $catalog->id]) }}">{{ __('messages.posts') }} <span id="c_posts"></a></div>
