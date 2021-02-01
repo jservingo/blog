f6fb7c96c366c4bd206578c1494968c570217bb2
Jorge Servin <jservingo@gmail.com>
2021-02-01 06:34:15 -0400

    show post

--- resources/views/posts/show_post.blade.php
+++ resources/views/posts/show_post.blade.php
@@ -58,7 +58,7 @@
 
 @push('styles')
   <!--<link rel="stylesheet" href="/css/twitter-bootstrap.css">-->
-  <link rel="stylesheet" href="/css/framework_single.css?ver=1.11">
+  <link rel="stylesheet" href="/css/framework_single.css?ver=1.12">
 @endpush
 
 @push('scripts')
