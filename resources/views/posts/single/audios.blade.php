dd50fac9bb7ac581fc609f3bb195286806c98ca3
Jorge Servin <jservingo@gmail.com>
2021-02-03 16:34:01 -0400

    single audios

--- resources/views/posts/single/audios.blade.php
+++ resources/views/posts/single/audios.blade.php
@@ -6,7 +6,7 @@
 		@foreach ($post->audios->sortBy('featured')->sortBy('position') as $audio)
 			<div style="background: #f4f4f4;">
 				<span style="color:#155597;font-weight:700;">{{ $audio->description }}</span>
-				<audio preload="auto" src="{{ url('storage/'.$audio->url) }}"></audio>
+				<audio preload="none" src="{{ url('storage/'.$audio->url) }}"></audio>
 			</div>
 		@endforeach
 	</div>
