3cc7c4824cae134433c3dcf39c48af044bcc346c
Jorge Servin <jservingo@gmail.com>
2021-03-25 20:10:34 -0400

    post full max height 220

--- resources/views/posts/full/view.blade.php
+++ resources/views/posts/full/view.blade.php
@@ -34,7 +34,7 @@
         </div>
         <div style="float:left;">
           @include('posts.full.title')
-          <div style="padding:4px 10px 4px 10px; text-align:justify; max-height:200px; overflow:hidden;">
+          <div style="padding:4px 10px 4px 10px; text-align:justify; max-height:220px; overflow:hidden;">
             @include('posts.full.content')
           </div>
         </div>        
@@ -70,7 +70,7 @@
         @include('posts.full.title')  
       </div>
       <div style="clear:both;"></div>
-      <div style="padding:4px 10px 0px 10px; text-align:justify; max-height:200px; overflow:hidden;">
+      <div style="padding:4px 10px 0px 10px; text-align:justify; max-height:220px; overflow:hidden;">
         @include('posts.full.content')
         <hr/>
       </div>
