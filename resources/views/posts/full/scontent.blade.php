d3ce0ee6acb11b3522885c13e817e87f6edf5ab5
Jorge Servin <jservingo@gmail.com>
2021-03-25 20:17:50 -0400

    post full max height body scontent

--- resources/views/posts/full/scontent.blade.php
+++ resources/views/posts/full/scontent.blade.php
@@ -1,4 +1,4 @@
 <div class="scontent"  
-			style="width:304px; background-color:{{ $zcolor }}; padding:2px 10px 10px 10px; text-align:justify;">
+			style="width:304px; background-color:{{ $zcolor }}; padding:2px 10px 10px 10px; text-align:justify; max-height:220px; overflow:hidden;">
   @include('posts.full.content')
 </div>
--- resources/views/posts/full/view.blade.php
+++ resources/views/posts/full/view.blade.php
@@ -76,7 +76,7 @@
       </div>
     </div>    
     @endif
-    <div style="padding:2px 10px 4px 10px; text-align:justify;">
+    <div style="padding:2px 10px 4px 10px; text-align:justify; max-height:220px; overflow:hidden;">
       {!! $post->body !!}
     </div>
     <div style="padding-left:10px;padding-right:10px;">
