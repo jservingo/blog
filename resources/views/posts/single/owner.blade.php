535835515b991b488b05791e12dfd9e60c12c4cb
Jorge Servin <jsetvingo@gmail.com>
2021-05-29 13:17:22 +0100

    posts sender date 2

--- resources/views/posts/single/owner.blade.php
+++ resources/views/posts/single/owner.blade.php
@@ -7,23 +7,33 @@
 <div class="image-w-text">
 	<span class="cite-2">{{ __('messages.owner') }}: </span>
   <span class="user c-blue">
-    <a id="t-user" href="/user/{{ $post->owner->id }}"></span> {{ $post->owner->name }}</a>
+    <a id="t-user" href="/user/{{ $post->owner->id }}"> 
+      {{ $post->owner->name }}
+    </a>
   </span>
 </div>
 
 <div class="image-w-text">
-  <span class="cite-2">{{ __('messages.publication-date') }}:</span> {{ $post->published_at->format('d/m/y') }}
+  <span class="cite-2">{{ __('messages.publication-date') }}:</span> 
+  {{ $post->published_at->format('d/m/y') }}
 </div> 
 
 @if ($post->published_at <> $post->updated_at)
 	<div class="image-w-text">
-  	<span class="cite-2">{{ __('messages.updated-date') }}:</span> {{ $post->updated_at->format('d/m/y') }}
+  	<span class="cite-2">{{ __('messages.updated-date') }}:</span>
+    {{ $post->updated_at->format('d/m/y') }}
 	</div> 
 @endif   
 
 @if ($post->kpost && ($post->kpost->send_by <> $post->kpost->user_id))
   <div class="image-w-text">
-    <span class="cite-2">{{ __('messages.sent-by') }}:</span> {{ $post->sender->name }} {{ $post->kpost->created_at->format('d/m/y') }}
+    <span class="cite-2">{{ __('messages.sent-by') }}:</span> 
+    <span class="user c-blue">
+      <a id="t-user" href="/user/{{ $post->sender->id }}">
+        {{ $post->sender->name }}
+      </a>
+    </span>
+    {{ $post->kpost->created_at->format('d/m/y') }}
   </div>
 @endif   
 
