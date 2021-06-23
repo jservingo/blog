09bf1f50ace2d16ba0be8e64b39b0cc6f450ae3f
Jorge Servin <jsetvingo@gmail.com>
2021-06-23 20:12:25 +0100

    post now 3

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -15,11 +15,13 @@ function format_num($n) {
   return round($n, max(0, 3 - strlen((int)$n))) ." $out";
 }
 
+/*
 function getUTCDate()
 {
   date_default_timezone_set('UTC');
   return ('Y-m-d H:i:s', time());
 }
+*/
 
 function create_session()
 {
