2d85524bbf7f1d8bb54c9cca0b75b30476c7a20a
Jorge Servin <jsetvingo@gmail.com>
2021-06-30 08:18:07 +0100

    timezoneOffset

--- app/functions/timezoneOffset.blade.php
+++ app/functions/timezoneOffset.blade.php
@@ -3,7 +3,8 @@
 	$timestamp_utc = floor(Carbon::now()->timestamp / 60000); 
 ?>
 <script>
-	var timestamp_utc = "<?php echo $timestamp_utc ?>";
+	var timestamp_utc = <?php echo $timestamp_utc ?>;
 	var timestamp_local = Math.floor(Date.now() / 60000);
 	var timezoneOffset = (timestamp_utc - timestamp_local) * 60000;
+	console.log(timezoneOffset);
 </script>
