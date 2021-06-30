94cf05e0affd69b18ce637016b4e9291d4116147
Jorge Servin <jsetvingo@gmail.com>
2021-06-30 19:24:00 +0100

    timezoneOffset 7

--- app/functions/timezoneOffset.blade.php
+++ app/functions/timezoneOffset.blade.php
@@ -1,11 +1,13 @@
 <?php 
 	use Carbon\Carbon;
-	$timestamp_utc = floor(Carbon::now()->getPreciseTimestamp(3) / 60000); 
+	$timestamp_utc = floor(Carbon::now()->timestamp); 
 ?>
 <script>
 	var timestamp_utc = <?php echo $timestamp_utc ?>;
-	var timestamp_local = Math.floor(new Date().getTime() / 60000);
-	var timezoneOffset = (timestamp_local - timestamp_utc) * 60000;
+	console.log(timestamp_utc);
+	var timestamp_local = Math.floor(new Date().getTime());
+	console.log(timestamp_local);
+	var timezoneOffset = timestamp_local - timestamp_utc;
 	console.log(timezoneOffset);
 
 	var d = new Date("2021-06-30 16:17:30 UTC");
