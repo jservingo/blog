35a5c780551263bf29bf7a40f14600a476f85c46
Jorge Servin <jsetvingo@gmail.com>
2021-06-30 21:20:02 +0100

    timezoneOffset 8

--- app/functions/timezoneOffset.blade.php
+++ app/functions/timezoneOffset.blade.php
@@ -5,7 +5,7 @@
 <script>
 	var timestamp_utc = <?php echo $timestamp_utc ?>;
 	console.log(timestamp_utc);
-	var timestamp_local = Math.floor(new Date().getTime());
+	var timestamp_local = Math.floor(new Date().getTime() / 1000);
 	console.log(timestamp_local);
 	var timezoneOffset = timestamp_local - timestamp_utc;
 	console.log(timezoneOffset);
