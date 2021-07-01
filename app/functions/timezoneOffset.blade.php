5a2d7d0588d26f5138fa309cf26dedea5f5d5fca
Jorge Servin <jsetvingo@gmail.com>
2021-07-01 07:58:28 +0100

    timezoneOffset 15

--- app/functions/timezoneOffset.blade.php
+++ app/functions/timezoneOffset.blade.php
@@ -10,8 +10,8 @@
 	var timezoneOffset = timestamp_local - timestamp_utc;
 	console.log(timezoneOffset);
 
-	var d = new Date("2021-06-30T16:17:30 UTC");
-	var timestamp = d.getTime()/1000;
+	var d = new Date("2021-06-30 16:17:30 UTC");
+	var timestamp = Math.floor(d.getTime()/1000);
 	console.log(timestamp);
     var date = new Date((timestamp + timezoneOffset)*1000);
     var fdate = date.toLocaleString('es-ES', {'year':'numeric', 'month':'2-digit', 'day':'2-digit', 'hour':'2-digit', 'minute':'2-digit', 'second':'2-digit' });
