e5c55c88dbb5c54193ec4e97f8f1ee64c7a6c721
Jorge Servin <jsetvingo@gmail.com>
2021-07-02 17:20:19 -0400

    functions fdateTimeEdit 3

--- public/js/functions.js
+++ public/js/functions.js
@@ -96,7 +96,7 @@ function fdateTimeEdit(d)
   var hh = date.getHours().toString();
   var ii = date.getMinutes().toString();
   var ss = date.getSeconds().toString(); 
-  return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0] + ' ' + (hh[1]?hh:"0"+hh[0]) + ':' + (ii[1]?ii:"0"+ii[0]) + ':' + (ss[1]?ss:"0"+ss[0]));
+  return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + ' ' + (hh[1]?hh:"0"+hh[0]) + ':' + (ii[1]?ii:"0"+ii[0]) + ':' + (ss[1]?ss:"0"+ss[0]);
 }
 
 //Convertir fecha y hora local un UTC
