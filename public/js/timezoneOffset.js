33eeadac3619f64607aff781f8d795e6bb3bfe69
Jorge Servin <jsetvingo@gmail.com>
2021-07-09 10:48:42 -0400

    timezoneOffset UTC

--- public/js/timezoneOffset.js
+++ public/js/timezoneOffset.js
@@ -1,6 +1,7 @@
 function get_timezoneOffset()
 {
-	var date_utc = new Date("2021-06-30 16:17:30 UTC");
+	//var date_utc = new Date("2021-06-30 16:17:30 UTC");
+	var date_utc = new Date(Date.UTC(2021, 6, 30, 16, 17, 30));
 	var timestamp_utc = Math.floor(date_utc.getTime() / 1000);
 	console.log(timestamp_utc);
 	var date_local = new Date("2021-06-30 16:17:30");
