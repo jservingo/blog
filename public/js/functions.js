59809a1f612f6abbdf197c68cd50f15ec933b899
Jorge Servin <jsetvingo@gmail.com>
2021-07-09 13:17:02 -0400

    functions timezoneOffset alerts

--- public/js/functions.js
+++ public/js/functions.js
@@ -116,7 +116,6 @@ function fdateTimeSaveUTC(d)
   var ii = date.getMinutes().toString();
   var ss = date.getSeconds().toString(); 
   var fdate = yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]) + ' ' + (hh[1]?hh:"0"+hh[0]) + ':' + (ii[1]?ii:"0"+ii[0]) + ':' + (ss[1]?ss:"0"+ss[0]);
-  alert(fdate);
   return fdate;
 }
 
--- public/js/timezoneOffset.js
+++ public/js/timezoneOffset.js
@@ -2,12 +2,12 @@ function get_timezoneOffset()
 {
 	var date_utc = new Date("2021-06-30 16:17:30 UTC");
 	var timestamp_utc = Math.floor(date_utc.getTime() / 1000);
-	console.log(timestamp_utc);
+	//console.log(timestamp_utc);
 	var date_local = new Date("2021-06-30 16:17:30");
 	var timestamp_local = Math.floor(date_local.getTime() / 1000);
-	console.log(timestamp_local);
+	//console.log(timestamp_local);
 	var timezoneOffset = timestamp_utc - timestamp_local;
-	console.log(timezoneOffset);
+	//console.log(timezoneOffset);
 	return timezoneOffset;
 }
 
