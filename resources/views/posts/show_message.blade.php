a663bb119abcb53fdfaeddc0714ddf4c4cc796bf
Jorge Servin <jsetvingo@gmail.com>
2021-07-12 06:02:43 -0400

    bgcolor #aac4d2

--- resources/views/posts/show_message.blade.php
+++ resources/views/posts/show_message.blade.php
@@ -131,7 +131,7 @@
 	}  
 @endphp
 
-<div style="width:90%; background-color:#fbfdf1; padding:10px; margin-bottom:20px;">
+<div style="width:90%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
 	<h4>{{ $msg_title }}</h4>
 	<p>{{ $msg_subtitle }}</p>
 </div>
