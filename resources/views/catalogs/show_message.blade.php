a64fea4d6ec456734176c892c82769b8613b6780
Jorge Servin <jsetvingo@gmail.com>
2021-07-12 07:10:45 -0400

    show message catalogs posts 2

--- resources/views/catalogs/show_message.blade.php
+++ resources/views/catalogs/show_message.blade.php
@@ -1,4 +1,4 @@
-<div style="width:90%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
+<div style="width:100%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
 	<h4 style="color:#fcfcf7">{{ $msg_title }}</h4>
 	<p style="color:#fcfcf7">{{ $msg_subtitle }}</p>
 </div>
--- resources/views/posts/show_message.blade.php
+++ resources/views/posts/show_message.blade.php
@@ -131,7 +131,7 @@
 	}  
 @endphp
 
-<div style="width:90%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
+<div style="width:100%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
 	<h4 style="color:#fcfcf7">{{ $msg_title }}</h4>
 	<p style="color:#fcfcf7">{{ $msg_subtitle }}</p>
 </div>
