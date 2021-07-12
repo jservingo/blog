be9fe968470539d24ccd9a7e549f403b4afabbed
Jorge Servin <jsetvingo@gmail.com>
2021-07-11 20:38:42 -0400

    catalogs ribbon script php 2

--- resources/views/catalogs/ribbon_script.blade.php
+++ resources/views/catalogs/ribbon_script.blade.php
@@ -16,6 +16,8 @@
 	  	  display: 4, // number of slides you want it to display at once
 	  	  loop: false // disable looping on slides
 		}); // this is all you need!
+
+		$(".container").width(1040);
 	@endforeach
 
 	@php
@@ -33,6 +35,7 @@
 	    		width: "300px",
 	    		display: 1
 				});
+
 				$(".container").width(300);			
 			}
 			if ($(window).width()>=768 && $(window).width()<992)
@@ -41,6 +44,7 @@
 	    		width: "540px",
 	    		display: 2
 				});
+
 				$(".container").width(540);			
 			}
 			if ($(window).width()>=992 && $(window).width()<1200)
@@ -48,7 +52,8 @@
 				$("#slider{{ $i }}").diyslider("updateOptions", {
 	    		width: "800px",
 	    		display: 3
-				});			
+				});		
+
 				$(".container").width(800);
 			}
 			if ($(window).width()>=1200)
@@ -56,7 +61,8 @@
 				$("#slider{{ $i }}").diyslider("updateOptions", {
 	    		width: "1040px",
 	    		display: 4
-				});			
+				});	
+						
 				$(".container").width(1040);
 			}
 	    //$("#jqxgrid").jqxGrid({ height: $(window).height() - 60 });
