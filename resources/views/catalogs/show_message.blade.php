0132bb2185e7776f49744d1a97fbf91b7e31a206
Jorge Servin <jsetvingo@gmail.com>
2021-07-12 06:40:23 -0400

    show message catalos, posts

--- resources/views/catalogs/show_message.blade.php
+++ resources/views/catalogs/show_message.blade.php
@@ -1,4 +1,4 @@
 <div style="width:90%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
-	<h4>{{ $msg_title }}</h4>
-	<p>{{ $msg_subtitle }}</p>
+	<h4 style="color:#fcfcf7">{{ $msg_title }}</h4>
+	<p style="color:#fcfcf7">{{ $msg_subtitle }}</p>
 </div>
--- resources/views/posts/show_message.blade.php
+++ resources/views/posts/show_message.blade.php
@@ -132,6 +132,6 @@
 @endphp
 
 <div style="width:90%; background-color:#aac4d2; padding:10px; margin-bottom:20px;">
-	<h4>{{ $msg_title }}</h4>
-	<p>{{ $msg_subtitle }}</p>
+	<h4 style="color:#fcfcf7">{{ $msg_title }}</h4>
+	<p style="color:#fcfcf7">{{ $msg_subtitle }}</p>
 </div>
