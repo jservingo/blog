104ee49c2e0df1f00322230a829fa4073b09c151
Jorge Servin <jsetvingo@gmail.com>
2021-07-15 07:34:05 -0400

    catalogs ribbon script php 51

--- resources/views/catalogs/ribbon_script.blade.php
+++ resources/views/catalogs/ribbon_script.blade.php
@@ -29,23 +29,23 @@
 			@php
 	    	$i = $i+1;
 			@endphp
-			if ($(window).width()<768)
+			if ($(window).width()<720)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
-	    		width: "256px",
+	    		width: "257px",
 	    		display: 1
 				});
 
-				$(".container").width(300);			
+				$(".posts.container").width(257);			
 			}
-			if ($(window).width()>=768 && $(window).width()<960)
+			if ($(window).width()>=720 && $(window).width()<960)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
 	    		width: "514px",
 	    		display: 2
 				});
 
-				$(".container").width(514);			
+				$(".posts.container").width(514);			
 			}
 			if ($(window).width()>=960 && $(window).width()<1200)
 			{
@@ -54,52 +54,52 @@
 	    		display: 3
 				});		
 
-				$(".container").width(771);
+				$(".posts.container").width(771);
 			}
-			if ($(window).width()>=1200 && $(window).width()<1600)
+			if ($(window).width()>=1200 && $(window).width()<1500)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
 	    		width: "1028px",
 	    		display: 4
 				});	
 
-				$(".container").width(1028);
+				$(".posts.container").width(1028);
 			}
-			if ($(window).width()>=1600 && $(window).width()<1900)
+			if ($(window).width()>=1500 && $(window).width()<1800)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
-	    		width: "1350px",
+	    		width: "1285px",
 	    		display: 5
 				});	
 
-				$(".container").width(1350);
+				$(".posts.container").width(1285);
 			}
-			if ($(window).width()>=1900 && $(window).width()<2100)
+			if ($(window).width()>=1800 && $(window).width()<2100)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
-	    		width: "1620px",
+	    		width: "1542px",
 	    		display: 6
 				});	
 
-				$(".container").width(1620);
+				$(".posts.container").width(1542);
 			}
-			if ($(window).width()>=2100 && $(window).width()<2500)
+			if ($(window).width()>=2100 && $(window).width()<2300)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
-	    		width: "1890px",
+	    		width: "1799px",
 	    		display: 7
 				});	
 
-				$(".container").width(1890);
+				$(".posts.container").width(1799);
 			}
-			if ($(window).width()>=2500)
+			if ($(window).width()>=2300)
 			{
 				$("#slider{{ $i }}").diyslider("updateOptions", {
-	    		width: "2160px",
+	    		width: "2056px",
 	    		display: 8
 				});	
 
-				$(".container").width(2160);
+				$(".posts.container").width(2056);
 			}
 	    //$("
 	    //$("#jqxgrid").jqxGrid({ height: $(window).height() - 60 });
