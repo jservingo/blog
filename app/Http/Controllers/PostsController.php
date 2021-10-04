4b4ef39463573dc87adc51decc79dd29dabf014e
Jorge Servin <jsetvingo@gmail.com>
2021-10-03 22:54:04 -0400

    edit post fmath 4

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -420,7 +420,7 @@ class PostsController extends Controller
       $body = html_entity_decode($body);
       $body = str_replace("<br />","",$body);
       $body = str_replace("<p>","",$body);
-      $body = str_replace("<p/>","",$body);
+      $body = str_replace("<p//>","",$body);
     }
     $post->body = $body;
     $post->url = $request->get('url');
