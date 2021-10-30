b9326238f0f2d85212f48e262a51e42d11494499
Jorge Servin <jsetvingo@gmail.com>
2021-10-29 21:19:51 -0400

    artists controller view topArtists 7

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -68,7 +68,9 @@ class ArtistsController extends Controller
 
     foreach($items as $artist)
     {
-      if(isset($artist->mbid) === true && $artist->mbid === '') {
+      $element = $artist->mbid;
+      $isEmpty = !count($element->xpath('(.)[./node()|./@*]'));
+      if($isEmpty) {
         fwrite($fp, "not found".",".$artist->name."\n");
       }
       if ($artist->mbid)      
