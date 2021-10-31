768c7136705dc77135b9ab3555cf41d6198b6349
Jorge Servin <jsetvingo@gmail.com>
2021-10-31 12:00:51 -0400

    artists controller view TopArtists 3

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -150,7 +150,7 @@ class ArtistsController extends Controller
 
     fclose($fp);
 
-    $content = \View::make('txt')->with('resp', $resp);
+    $content = \View::make('resp')->with('resp', $resp);
     return \Response::make($content, '200')->header('Content-Type', 'plain/txt');
   }
 
