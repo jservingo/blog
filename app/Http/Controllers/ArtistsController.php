18af1a6ac2db406f292f4894dfc756c389bba5ff
Jorge Servin <jsetvingo@gmail.com>
2021-11-13 08:53:13 -0400

    artists controller create posts 47

--- app/Http/Controllers/ArtistsController.php
+++ app/Http/Controllers/ArtistsController.php
@@ -119,35 +119,7 @@ class ArtistsController extends Controller
       foreach($tags_artist->children() as $tag) {
         $tags = $tags.",".$tag->name;
       } 
-
-      $url_artist = "http://musicbrainz.org/ws/2/artist/".$mbid."?inc=url-rels";
       
-      $curl = curl_init();
-      curl_setopt_array($curl, Array(
-        CURLOPT_URL            => $url_artist,
-        CURLOPT_USERAGENT      => "Kodelia/1.0 (jservingo@gmail.com)",
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_ENCODING       => 'UTF-8'
-      ));      
-      $data = curl_exec($curl);
-      curl_close($curl);
-
-      if ($data[0] == "<" && $data[1] == "/") {
-        $xml = simplexml_load_string($data);
-        
-        $links_artist = $xml->{'artist'}->{'relation-list'};
-        foreach($links_artist->children() as $link) {
-          $links = $links."<a href='".$link->target."' target='_blank'>".$link->attributes()->{'type'}."</a> ";
-          if ($link->attributes()->{'type'} == "image")
-            $url_image = $link->{'target'}; 
-        }
-
-        /*
-        if ($url_image != "")
-          $img = get_post_image($url_image);
-        */
-      }
-
       //Buscar post de la app
       $post = Post
         ::where("app_id","=",$app_id)
