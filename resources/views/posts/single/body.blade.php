37604f4eaaead4c35c00852294fe32387663c221
Jorge Servin <jsetvingo@gmail.com>
2022-05-01 12:21:28 -0400

    single post h2

--- resources/views/posts/single/body.blade.php
+++ resources/views/posts/single/body.blade.php
@@ -1,4 +1,4 @@
-<div class="image-w-text" style="background-color:#566573; color:white; padding:10px; margin-top:10px; border:10px solid #7FB3D5;font-family:cursive; font-size:20px;">
+<div class="image-w-text" style="background-color:#2662DF; color:white; padding:10px; margin-top:10px; border:10px solid #7FB3D5;font-family:cursive; font-size:20px;">
   @if ($post->kpost && $post->kpost->excerpt)
     {{ $post->kpost->excerpt }}
   @else
