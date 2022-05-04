8340e92d06e1dc60d4aef5be8afa33d50d4f205b
Jorge Servin <jsetvingo@gmail.com>
2022-05-04 19:36:37 -0400

    single kpub 5

--- public/js/kpub.js
+++ public/js/kpub.js
@@ -2,13 +2,17 @@ var kpub_page = 0;
 
 function show_kpub(page)
 {
+	console.log("show page "+page);
 	element = $(".kpub[page="+page+"]");
 	if (element && (kpub_page != page))
 	{
-		var old_element = $(".kpub[page="+kpub_page+"]");
-		if (old_element)
+		if (kpub_page>=1)
+		{
+			var old_element = $(".kpub[page="+kpub_page+"]");
 			old_element.hide();
+		}
 		element.show();
+		console.log(element);
 		kpub_page = page;		
 	}
 }
@@ -20,9 +24,11 @@ function btn_kpub_prev() {
 
 
 function btn_kpub_next() {
+	console.log("next");
 	show_kpub(kpub_page+1);
 }
 
 $(function() {
+	console.log("page=1");
 	show_kpub(1);
 });
