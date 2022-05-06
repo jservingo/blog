7248ec300e3a503667fd4f40795c73ac6154c2b5
Jorge Servin <jsetvingo@gmail.com>
2022-05-06 16:59:18 -0400

    single kpub 8

--- resources/views/posts/single/body.blade.php
+++ resources/views/posts/single/body.blade.php
@@ -1,4 +1,4 @@
-<div class="image-w-text" style="background-color:#2662DF; color:white; padding:10px; margin-top:10px; border:10px solid #7FB3D5;font-family:cursive; font-size:20px;">
+<div style="background-color:#2662DF; color:white; padding:10px; margin-top:10px; border:10px solid #7FB3D5;font-family:cursive; font-size:20px;">
   @if ($post->kpost && $post->kpost->excerpt)
     {{ $post->kpost->excerpt }}
   @else
@@ -6,7 +6,7 @@
   @endif
 </div>
 
-<div class="image-w-text" style="background-color:#7FB3D5; color:white; padding:10px; margin-top:10px; font-size:18px;">
+<div style="background-color:#7FB3D5; color:white; padding:10px; margin-top:10px; font-size:18px;">
   @php
     if ($post->source == "@kpub")
     {
