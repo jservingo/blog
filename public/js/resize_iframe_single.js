9be0a43d2b1b689ca9daf484989b0cee8b8b0bb2
Jorge Servin <jsetvingo@gmail.com>
2022-05-12 18:26:35 -0400

    show single kpub 25

--- public/js/resize_iframe_single.js
+++ public/js/resize_iframe_single.js
@@ -11,7 +11,7 @@ $allVideos.each(function() {
 
 // When the window is resized
 $(window).resize(function() {
-  var container_width = $("#main_panel").width()-62;
+  var container_width = $("#main_panel").width()-42;
   var newWidth = Math.min(600,container_width);
   
   // Resize all videos according to their own aspect ratio
