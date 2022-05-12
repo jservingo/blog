2d03dc58d82127ac52be377fdaeca44735bc27f0
Jorge Servin <jsetvingo@gmail.com>
2022-05-12 17:15:34 -0400

    show single excerpt content 3

--- resources/views/posts/single/content.blade.php
+++ resources/views/posts/single/content.blade.php
@@ -1,11 +1,13 @@
-<div id="content-post" style="background-color:#7FB3D5; padding:10px; margin-top:10px;"> 
-  @if ($post->iframe)
-    @include('posts.single.iframe')
-  @elseif ($post->photos->count() === 1)
-    @if ($post->source != "@kpub")
-      @include('posts.single.photo')
-    @endif
-  @elseif ($post->photos->count() > 1)
-    @include('posts.box.carousel')       
-  @endif   
-</div>
+@if ($post->iframe || $post->photos->count() === 1 || $post->photos->count() > 1)
+  <div id="content-post" style="background-color:#7FB3D5; padding:10px; margin-top:10px;"> 
+    @if ($post->iframe)
+      @include('posts.single.iframe')
+    @elseif ($post->photos->count() === 1)
+      @if ($post->source != "@kpub")
+        @include('posts.single.photo')
+      @endif
+    @elseif ($post->photos->count() > 1)
+      @include('posts.box.carousel')       
+    @endif   
+  </div>
+@endif
