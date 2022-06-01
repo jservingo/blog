35b35caaa4ad2876e3f827a8f27fddd503771434
Jorge Servin <jsetvingo@gmail.com>
2022-06-01 17:48:56 -0400

    show post epub 9

--- resources/views/posts/show_post.blade.php
+++ resources/views/posts/show_post.blade.php
@@ -184,6 +184,7 @@
       if ($("#epub").length)
       {
         var source = $("#epub").data("source"); 
+        console.log(source);
         var book = ePub(source);
         var rendition = book.renderTo("epub");
         //var rendition = book.renderTo("epub", {method:"continuous", flow:"scrolled", width:600, height:400});
