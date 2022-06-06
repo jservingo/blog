5891e535fe051e8e4d3519bdcbcd99b438861454
Jorge Servin <jsetvingo@gmail.com>
2022-06-06 09:01:13 -0400

    show single epub 22

--- resources/views/posts/show_post.blade.php
+++ resources/views/posts/show_post.blade.php
@@ -184,6 +184,7 @@
     $(function() {
       if ($("#area").length)
       {
+        /*
         console.log("get source");
         var source = $("#area").data("source"); 
         console.log(source);
@@ -191,7 +192,61 @@
         var rendition = book.renderTo("area", {width:600, height:400});
         rendition.display();
         console.log("End epub");
-        //var rendition = book.renderTo("epub", {method:"continuous", flow:"scrolled", width:600, height:400});
+        */
+        var $viewer = document.getElementById("viewer");
+        var $next = document.getElementById("next");
+        var $prev = document.getElementById("prev");
+        var currentSection;
+        var currentSectionIndex = 6;
+
+        var book = ePub("https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf");
+        book.loaded.navigation.then(function(toc){
+          var $select = document.getElementById("toc"),
+              docfrag = document.createDocumentFragment();
+
+          toc.forEach(function(chapter) {
+            var option = document.createElement("option");
+            option.textContent = chapter.label;
+            option.ref = chapter.href;
+
+            docfrag.appendChild(option);
+          });
+
+          $select.appendChild(docfrag);
+
+          $select.onchange = function(){
+              var index = $select.selectedIndex,
+                  url = $select.options[index].ref;
+              display(url);
+              return false;
+          };
+
+          book.opened.then(function(){
+            display(currentSectionIndex);
+          });
+
+          $next.addEventListener("click", function(){
+            var displayed = display(currentSectionIndex+1);
+            if(displayed) currentSectionIndex++;
+          }, false);
+
+          $prev.addEventListener("click", function(){
+            var displayed = display(currentSectionIndex-1);
+            if(displayed) currentSectionIndex--;
+          }, false);
+
+          function display(item){
+            var section = book.spine.get(item);
+            if(section) {
+              currentSection = section;
+              section.render().then(function(html){
+                // $viewer.srcdoc = html;
+                $viewer.innerHTML = html;
+              });
+            }
+            return section;
+          }
+        });
       } 
     });   
   </script>
